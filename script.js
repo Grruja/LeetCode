944f330d23adc1ed92a0d0f69cb2b800a76d2d23
AleexxG <alexanderg.sln@gmail.com>
2023-02-20 14:05:44 +0100

    Best Time to Buy and Sell Stock

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
-/* ----- TWO SUM ----- */
+/* ----- Two Sum ----- */
 
-const twoSum = (nums, target) => 
+const two_sum = (nums, target) => 
 {
     const map = new Map();
     
@@ -18,4 +18,30 @@ const twoSum = (nums, target) =>
 
         map.set(nums[i], i);
     }
+}
+
+
+/* ----- Best Time to Buy and Sell Stock ----- */
+
+const buy_stock = (price) =>
+{
+    let buy = price[0];
+    price[0] = 0;
+    let profit = 0;
+
+    for (let i = 1; i < price.length; i++) 
+    {
+        if (buy > price[i]) 
+        {
+            buy = price[i];
+            price[i] = 0;
+            profit = 0;
+        } 
+        else 
+        {
+            profit = Math.max(...price) - buy;
+        }
+    }
+
+    return profit;
 }
