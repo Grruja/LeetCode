53afe1ab91f4aac6aa7287a5e040d2e83b3ac353
AleexxG <alexanderg.sln@gmail.com>
2023-04-06 17:27:42 +0200

    Maximum Product Subarray

--- script.js
+++ script.js
@@ -113,3 +113,17 @@ function maxSubArray(nums) {
     
     return solution;
 }
+
+
+/* ----- Maximum Product Subarray ----- */
+
+function maxProduct(nums) {
+    let solution = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] = Math.max(nums[i], nums[i] * nums[i-1]);
+        solution = Math.max(solution, nums[i]);
+    }
+    
+    return solution;
+}
