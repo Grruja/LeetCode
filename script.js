4324b458ad83bee546ec352d358fe147c4944b37
AleexxG <alexanderg.sln@gmail.com>
2023-05-16 19:01:05 +0200

    Unique paths

--- script.js
+++ script.js
@@ -438,18 +438,33 @@ function rob(nums) {
 
 function numDecodings(s) {
     if (s[0] === '0') return 0;
-
+    
     const howManyDecodes = Array(s.length + 1).fill(0);
     howManyDecodes[0] = 1;
     howManyDecodes[1] = 1;
-
+    
     for (let i = 2; i <= s.length; i++) {
         const single = s.slice(i - 1, i);
         const double = s.slice(i - 2, i);
-
+        
         if (single > 0) howManyDecodes[i] = howManyDecodes[i - 1];
         if (double >= 10 && double <= 26) howManyDecodes[i] += howManyDecodes[i - 2];
     }
-
+    
     return howManyDecodes[s.length];
+}
+
+
+/* ----- Unique paths ----- */
+
+function uniquePaths(n, m) {
+    const memo = Array(m).fill(Array(n).fill(1));
+
+    for (let row = 1; row < m; row++) {
+        for (let col = 1; col < n; col++) {
+            memo[row][col] = memo[row - 1][col] + memo[row][col - 1];
+        }
+    }
+
+    return memo[m - 1][n - 1];
 }
