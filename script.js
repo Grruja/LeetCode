ddabbd2d56b1b108b96d629b77bfe01354b5ef78
AleexxG <alexanderg.sln@gmail.com>
2023-05-23 19:15:16 +0200

    Length of Last Word

--- script.js
+++ script.js
@@ -490,23 +490,32 @@ function insertInterval(intervals, newInterval) {
     let i = 0;
     const start = 0;
     const end = 1;
-
+    
     while (i < intervals.length && intervals[i][end] < newInterval[start]) {
         res.push(intervals[i]);
         i++;
     }
-
+    
     while (i < intervals.length && intervals[i][start] <= newInterval[end]) {
         newInterval[start] = Math.min(newInterval[start], intervals[i][start]);
         newInterval[end] = Math.max(newInterval[end], intervals[i][end]);
         i++;
     }
     res.push(newInterval);
-
+    
     while (i < intervals.length) {
         res.push(intervals[i]);
         i++;
     }
-
+    
     return res;
+}
+
+
+/* ----- Length of Last Word ----- */
+
+function lengthOfLastWord(s) {
+    s = s.trim();
+    const words = s.split(" ");
+    return words[words.length - 1].length;
 }
